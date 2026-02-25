c176d9500a44035196d313264a70f6d6b3064c7e
OlofEricsson <wsl@5CD3147JW7.learnet.se>
2026-02-25 10:04:18 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<h1 class="text-white text-center font-bold fontsans text-4xl p-5">Balance point</h1>
+
+<div class="grid grid-cols-[1fr_60vw_1fr] 
+		h-screen">
+	<div class="col-start-2 col-end-2 text-white">
+
+		{@render children()}
+
+	</div>
+
+</div>
