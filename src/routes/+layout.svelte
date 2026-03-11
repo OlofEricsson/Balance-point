88df5583865a30daf9902ccc8b086923d1070401
OlofEricsson <wsl@5CD3147JW7.learnet.se>
2026-03-11 10:18:13 +0100

    extensive work to game page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,16 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<h1 class="text-white text-center font-bold fontsans text-4xl p-5">Balance point</h1>
+<div data-sveltekit-preload-data="hover" data-sveltekit-preload-code="viewport" class="flex flex-row ">
+	<div class="text-white text-center font-bold fontsans text-4xl p-5 absolute left-1/2 -translate-x-1/2">
+		<a href="/home">Balance point</a>
+	</div>
+	<div class="text-white text-right font-bold fontsans text-4xl p-5 place-self-end ml-auto">
+		<a href="/game">game</a>
+	</div>
+</div>
 
-<div class="grid grid-cols-[1fr_60vw_1fr] 
-		h-screen">
+<div class="grid grid-cols-[1fr_60vw_1fr] h-screen">
 	<div class="col-start-2 col-end-2 text-white">
 
 		{@render children()}
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const canvas = document.querySelector('#myCanvas');
+		const ctx = canvas.getContext('2d');
+
+		ground(ctx);
+		wheel(ctx);
+
+	});
+
+	function wheel(ctx) {
+		ctx.beginPath();
+		ctx.arc(200, 575, 50, 0, 2 * Math.PI);
+		ctx.fillStyle = 'black'; //funkar ej
+		ctx.stroke();
+	}
+
+	function ground(ctx) {
+		ctx.beginPath();
+		ctx.fillStyle = 'rgb(100, 100, 100)';
+		ctx.fillRect(0, 500, 1000, 200);
+	}
+
+</script>
+
+<p>hej hej hallå</p>
+
+<canvas
+	id="myCanvas"
+	class="m-auto bg-blue-400"
+	width="1000"
+	height="700"
+	style="image-rendering: pixelated;"
+></canvas>
+
+<style>
+	canvas {
+		border: 2px solid rgb(255, 255, 255);
+	}
+</style>
